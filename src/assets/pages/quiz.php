c1d847ed0a390b5b275378c835711b65da7782b3
Geovana Borba <geborba19@hotmail.com>
2023-05-03 14:43:35 -0300

    nav personalizado incluído no css do quiz, visto que havia bug com a estilização geral apenas na pagina do jogo

--- src/assets/pages/quiz.php
+++ src/assets/pages/quiz.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="../../css/quiz.css">
-    <link rel="stylesheet" href="../../css/nav.css">
     <link rel="stylesheet" href="../../css/reset.css">
     <link rel="stylesheet" href="../../css/scroll.css">
 
