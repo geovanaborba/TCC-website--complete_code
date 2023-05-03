e8d7640fe125a68eb0b1b1bc1f52efd3bef92096
EduardoGuerts <eduardoguerts@hotmail.com>
2023-05-03 04:25:15 -0300

    adicionado verificação de cadastro já existente

--- src/assets/pages/cadastroAction.php
+++ src/assets/pages/cadastroAction.php
@@ -22,22 +22,37 @@
 
         require_once('./conexao.php');
 
-        $sql = "INSERT INTO cadastro (nome, username, email,senha)
-                VALUES ('" . $_POST['txtNome'] . "', '" . $_POST['txtUsername'] . "','" . $_POST['txtEmail'] . "', '" . $_POST['txtSenha'] . "')";
+        // verifica se o usuário já existe
+        $sql = "SELECT * FROM cadastro WHERE email = '" . $_POST['txtEmail'] . "' OR username = '" . $_POST['txtUsername'] . "'";
+        $result = $conexao->query($sql);
 
-        if ($conexao->query($sql) === TRUE) {
+        if ($result->num_rows > 0) {
+            // usuário já cadastrado, exibe mensagem de erro
             echo '
-                <h1 class="titulo-ok">Cadastro Realizado! <br> Por favor, efetue o login para validar seus dados.</h1>
-                    <a href="./login.php">
-                        <button class="btn-ok"> OK </button>
-                    </a>
+                <h1 class="titulo-erro">ERRO! <br>Usuário já cadastrado.</h1>
+                <a href="./cadastro.php">
+                    <button class="btn-erro"> OK </button>
+                </a>
             ';
         } else {
+            // insere os dados na tabela
+            $sql = "INSERT INTO cadastro (nome, username, email, senha)
+                    VALUES ('" . $_POST['txtNome'] . "', '" . $_POST['txtUsername'] . "','" . $_POST['txtEmail'] . "', '" . $_POST['txtSenha'] . "')";
+            // mensagem de sucesso
+            echo '
+                <h1 class="titulo-ok">Cadastro Realizado! <br> Por favor, efetue o login para validar seus dados.</h1>
+                <a href="./login.php">
+                    <button class="btn-ok"> OK </button>
+                </a>
+            ';  
+        }
+
+        if ($conexao->query($sql) === FALSE) {
             echo '
                 <h1 class="titulo-erro">ERRO! <br>Tente novamente. </h1>
-                    <a href="./cadastro.php">
-                        <button class="btn-erro"> OK </button>
-                    </a>
+                <a href="./cadastro.php">
+                    <button class="btn-erro"> OK </button>
+                </a>
             ';
         }
 
